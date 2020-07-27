d401e75fd0c4c5f7451c37ccd2cecf38fbacb659
makoto <makoto@DESKTOP-IO0229E.lan>
2020-07-27 11:29:03 +0800

    test

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("修改后重新提交GitHub测试。");
 	}
 
 }
